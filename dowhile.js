068b204a19a1e63e7e1257525f412120b02310d4
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-05-28 10:50:38 +0530

    for and while loop

---
+++ dowhile.js
@@ -0,0 +1,5 @@
+let counter=1;
+do{
+    console.log("counter:"+counter)
+    counter ++;
+}while(counter>5)
---
+++ forinloop.js
@@ -0,0 +1,4 @@
+const person={name:"Sarah",Age:"23",City:"New York"}
+for(let property in person){
+    console.log(property+":"+person[property])
+}
